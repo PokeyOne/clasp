860e959c7dd2279bfbfc0511766201d857b1710c
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-16 02:33:57 -0700

    Licensing

--- src/compiler/assembly_compiler.c
+++ src/compiler/assembly_compiler.c
@@ -1,3 +1,20 @@
+/* Copyright (c) 2021 Mateo Carreras
+
+   This file is part of The CLASP Programming Language.
+
+   CLASP is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3 of the License.
+
+   CLASP is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have recieved a copy of the GNU General Public License
+   along with Bash. If not, see <http://www.gnu.org/licenses/>
+*/
+
 #include "assembly_compiler.h"
 
 void skip_whitespace(TrackedString* src) {
