ebd459673e466e8ae6f77f0c218a8a17fddb8e23
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-29 00:54:16 -0700

    Hypothetically working memory read methods, untested but compiles

--- rust_version/src/memory.rs
+++ rust_version/src/memory.rs
@@ -3,34 +3,64 @@ use std::convert::TryInto;
 /// The number of bytes in a word
 const WORD_SIZE: usize = 8;
 
+#[allow(dead_code)]
+const REGISTER_COUNT: u64 = 26;
+#[allow(dead_code)]
 const REGISTER_MEMORY_START: MemoryLocation = 0x8000_0000_0000_0000;
+#[allow(dead_code)]
 const RMS: MemoryLocation = REGISTER_MEMORY_START;
-const GA_LOC: MemoryLocation = RMS + WORD_SIZE * 0;
-const GB_LOC: MemoryLocation = RMS + WORD_SIZE * 1;
-const GC_LOC: MemoryLocation = RMS + WORD_SIZE * 2;
-const GD_LOC: MemoryLocation = RMS + WORD_SIZE * 3;
-const GE_LOC: MemoryLocation = RMS + WORD_SIZE * 4;
-const GF_LOC: MemoryLocation = RMS + WORD_SIZE * 5;
-const GG_LOC: MemoryLocation = RMS + WORD_SIZE * 6;
-const GH_LOC: MemoryLocation = RMS + WORD_SIZE * 7;
-const GI_LOC: MemoryLocation = RMS + WORD_SIZE * 8;
-const GJ_LOC: MemoryLocation = RMS + WORD_SIZE * 9;
-const GK_LOC: MemoryLocation = RMS + WORD_SIZE * 10;
-const GL_LOC: MemoryLocation = RMS + WORD_SIZE * 11;
-const GM_LOC: MemoryLocation = RMS + WORD_SIZE * 12;
-const GN_LOC: MemoryLocation = RMS + WORD_SIZE * 13;
-const GO_LOC: MemoryLocation = RMS + WORD_SIZE * 14;
-const GP_LOC: MemoryLocation = RMS + WORD_SIZE * 15;
-const GQ_LOC: MemoryLocation = RMS + WORD_SIZE * 16;
-const GR_LOC: MemoryLocation = RMS + WORD_SIZE * 17;
-const GS_LOC: MemoryLocation = RMS + WORD_SIZE * 18;
-const GT_LOC: MemoryLocation = RMS + WORD_SIZE * 19;
-const GU_LOC: MemoryLocation = RMS + WORD_SIZE * 20;
-const GV_LOC: MemoryLocation = RMS + WORD_SIZE * 21;
-const GW_LOC: MemoryLocation = RMS + WORD_SIZE * 22;
-const GX_LOC: MemoryLocation = RMS + WORD_SIZE * 23;
-const GY_LOC: MemoryLocation = RMS + WORD_SIZE * 24;
-const GZ_LOC: MemoryLocation = RMS + WORD_SIZE * 25;
+#[allow(dead_code)]
+const GA_LOC: MemoryLocation = RMS + (WORD_SIZE * 0) as u64;
+#[allow(dead_code)]
+const GB_LOC: MemoryLocation = RMS + (WORD_SIZE * 1) as u64;
+#[allow(dead_code)]
+const GC_LOC: MemoryLocation = RMS + (WORD_SIZE * 2) as u64;
+#[allow(dead_code)]
+const GD_LOC: MemoryLocation = RMS + (WORD_SIZE * 3) as u64;
+#[allow(dead_code)]
+const GE_LOC: MemoryLocation = RMS + (WORD_SIZE * 4) as u64;
+#[allow(dead_code)]
+const GF_LOC: MemoryLocation = RMS + (WORD_SIZE * 5) as u64;
+#[allow(dead_code)]
+const GG_LOC: MemoryLocation = RMS + (WORD_SIZE * 6) as u64;
+#[allow(dead_code)]
+const GH_LOC: MemoryLocation = RMS + (WORD_SIZE * 7) as u64;
+#[allow(dead_code)]
+const GI_LOC: MemoryLocation = RMS + (WORD_SIZE * 8) as u64;
+#[allow(dead_code)]
+const GJ_LOC: MemoryLocation = RMS + (WORD_SIZE * 9) as u64;
+#[allow(dead_code)]
+const GK_LOC: MemoryLocation = RMS + (WORD_SIZE * 10) as u64;
+#[allow(dead_code)]
+const GL_LOC: MemoryLocation = RMS + (WORD_SIZE * 11) as u64;
+#[allow(dead_code)]
+const GM_LOC: MemoryLocation = RMS + (WORD_SIZE * 12) as u64;
+#[allow(dead_code)]
+const GN_LOC: MemoryLocation = RMS + (WORD_SIZE * 13) as u64;
+#[allow(dead_code)]
+const GO_LOC: MemoryLocation = RMS + (WORD_SIZE * 14) as u64;
+#[allow(dead_code)]
+const GP_LOC: MemoryLocation = RMS + (WORD_SIZE * 15) as u64;
+#[allow(dead_code)]
+const GQ_LOC: MemoryLocation = RMS + (WORD_SIZE * 16) as u64;
+#[allow(dead_code)]
+const GR_LOC: MemoryLocation = RMS + (WORD_SIZE * 17) as u64;
+#[allow(dead_code)]
+const GS_LOC: MemoryLocation = RMS + (WORD_SIZE * 18) as u64;
+#[allow(dead_code)]
+const GT_LOC: MemoryLocation = RMS + (WORD_SIZE * 19) as u64;
+#[allow(dead_code)]
+const GU_LOC: MemoryLocation = RMS + (WORD_SIZE * 20) as u64;
+#[allow(dead_code)]
+const GV_LOC: MemoryLocation = RMS + (WORD_SIZE * 21) as u64;
+#[allow(dead_code)]
+const GW_LOC: MemoryLocation = RMS + (WORD_SIZE * 22) as u64;
+#[allow(dead_code)]
+const GX_LOC: MemoryLocation = RMS + (WORD_SIZE * 23) as u64;
+#[allow(dead_code)]
+const GY_LOC: MemoryLocation = RMS + (WORD_SIZE * 24) as u64;
+#[allow(dead_code)]
+const GZ_LOC: MemoryLocation = RMS + (WORD_SIZE * 25) as u64;
 
 /// Just an alias for word to be more descriptive when referring to a location
 pub type MemoryLocation = u64;
@@ -38,6 +68,7 @@ pub type MemoryLocation = u64;
 pub type Word = u64;
 /// An alias for the u8 type to signal when working with data
 pub type Byte = u8;
+pub type WordByteArray = [Byte; WORD_SIZE];
 
 /// ByteCollection is a trait exclusively meant for the Word type to implement
 /// a couple useful functions/methods.
@@ -70,7 +101,7 @@ impl ByteCollection for Word {
     }
 
     fn get_bytes(&self) -> [Byte; WORD_SIZE] {
-        let mut temp = self.clone();
+        let temp = self.clone();
         let mut result: [Byte; WORD_SIZE] = [0 as Byte; WORD_SIZE];
 
         for i in 0..8 {
@@ -86,12 +117,46 @@ impl ByteCollection for Word {
 
 pub struct Memory {
     memory_size: u64,
-    memory: Vec<Byte>
+    memory: Vec<Byte>,
+    registers: [Word; REGISTER_COUNT as usize]
 }
 
 impl Memory {
-    pub fn read(location: MemoryLocation) -> Word {
-        return 0;
+    pub fn new(size: u64) -> Memory {
+        let mut memory: Vec<Byte> = Vec::new();
+        memory.resize(size as usize, 0 as Byte);
+
+        Memory {
+            memory_size: size,
+            memory: memory,
+            registers: [0 as Word; REGISTER_COUNT as usize]
+        }
+    }
+
+    pub fn read(&self, location: MemoryLocation) -> Word {
+        if location >= RMS {
+            self.read_register(location)
+        } else {
+            // Might be more efficient to store memory as words and
+            // editing bytes takes longer, because words will be most common
+            let mut bytes: WordByteArray = [0; WORD_SIZE];
+            for i in 0..8 {
+                bytes[i] = self.memory[i + location as usize];
+            }
+
+            Word::from_bytes(&bytes)
+        }
+    }
+
+    fn read_register(&self, location: MemoryLocation) -> Word {
+        if location % WORD_SIZE as u64 != 0 {
+            panic!() // TODO: Actually handle this. Some sort of RuntimeError type. Maybe a custom Result for runtime environment
+        }
+
+        let index = (location - RMS) / WORD_SIZE as u64;
+
+        // TODO: Also return error for out of bounds
+        self.registers[index as usize]
     }
 }
 
