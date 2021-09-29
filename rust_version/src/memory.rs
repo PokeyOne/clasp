b9879bb74fcf4c33758de6b88ca5ac1301a4878f
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-29 01:09:25 -0700

    Moved memory constants and types to their own files to improve readability

--- rust_version/src/memory.rs
+++ rust_version/src/memory.rs
@@ -1,74 +1,9 @@
+pub mod constants;
+pub mod types;
+
+use constants::*;
+use types::*;
 use std::convert::TryInto;
-
-/// The number of bytes in a word
-const WORD_SIZE: usize = 8;
-
-#[allow(dead_code)]
-const REGISTER_COUNT: u64 = 26;
-#[allow(dead_code)]
-const REGISTER_MEMORY_START: MemoryLocation = 0x8000_0000_0000_0000;
-#[allow(dead_code)]
-const RMS: MemoryLocation = REGISTER_MEMORY_START;
-#[allow(dead_code)]
-const GA_LOC: MemoryLocation = RMS + (WORD_SIZE * 0) as u64;
-#[allow(dead_code)]
-const GB_LOC: MemoryLocation = RMS + (WORD_SIZE * 1) as u64;
-#[allow(dead_code)]
-const GC_LOC: MemoryLocation = RMS + (WORD_SIZE * 2) as u64;
-#[allow(dead_code)]
-const GD_LOC: MemoryLocation = RMS + (WORD_SIZE * 3) as u64;
-#[allow(dead_code)]
-const GE_LOC: MemoryLocation = RMS + (WORD_SIZE * 4) as u64;
-#[allow(dead_code)]
-const GF_LOC: MemoryLocation = RMS + (WORD_SIZE * 5) as u64;
-#[allow(dead_code)]
-const GG_LOC: MemoryLocation = RMS + (WORD_SIZE * 6) as u64;
-#[allow(dead_code)]
-const GH_LOC: MemoryLocation = RMS + (WORD_SIZE * 7) as u64;
-#[allow(dead_code)]
-const GI_LOC: MemoryLocation = RMS + (WORD_SIZE * 8) as u64;
-#[allow(dead_code)]
-const GJ_LOC: MemoryLocation = RMS + (WORD_SIZE * 9) as u64;
-#[allow(dead_code)]
-const GK_LOC: MemoryLocation = RMS + (WORD_SIZE * 10) as u64;
-#[allow(dead_code)]
-const GL_LOC: MemoryLocation = RMS + (WORD_SIZE * 11) as u64;
-#[allow(dead_code)]
-const GM_LOC: MemoryLocation = RMS + (WORD_SIZE * 12) as u64;
-#[allow(dead_code)]
-const GN_LOC: MemoryLocation = RMS + (WORD_SIZE * 13) as u64;
-#[allow(dead_code)]
-const GO_LOC: MemoryLocation = RMS + (WORD_SIZE * 14) as u64;
-#[allow(dead_code)]
-const GP_LOC: MemoryLocation = RMS + (WORD_SIZE * 15) as u64;
-#[allow(dead_code)]
-const GQ_LOC: MemoryLocation = RMS + (WORD_SIZE * 16) as u64;
-#[allow(dead_code)]
-const GR_LOC: MemoryLocation = RMS + (WORD_SIZE * 17) as u64;
-#[allow(dead_code)]
-const GS_LOC: MemoryLocation = RMS + (WORD_SIZE * 18) as u64;
-#[allow(dead_code)]
-const GT_LOC: MemoryLocation = RMS + (WORD_SIZE * 19) as u64;
-#[allow(dead_code)]
-const GU_LOC: MemoryLocation = RMS + (WORD_SIZE * 20) as u64;
-#[allow(dead_code)]
-const GV_LOC: MemoryLocation = RMS + (WORD_SIZE * 21) as u64;
-#[allow(dead_code)]
-const GW_LOC: MemoryLocation = RMS + (WORD_SIZE * 22) as u64;
-#[allow(dead_code)]
-const GX_LOC: MemoryLocation = RMS + (WORD_SIZE * 23) as u64;
-#[allow(dead_code)]
-const GY_LOC: MemoryLocation = RMS + (WORD_SIZE * 24) as u64;
-#[allow(dead_code)]
-const GZ_LOC: MemoryLocation = RMS + (WORD_SIZE * 25) as u64;
-
-/// Just an alias for word to be more descriptive when referring to a location
-pub type MemoryLocation = u64;
-/// The standard unit of the system == 8 bytes == 64 bit
-pub type Word = u64;
-/// An alias for the u8 type to signal when working with data
-pub type Byte = u8;
-pub type WordByteArray = [Byte; WORD_SIZE];
 
 /// ByteCollection is a trait exclusively meant for the Word type to implement
 /// a couple useful functions/methods.
---
+++ rust_version/src/memory/types.rs
@@ -0,0 +1,9 @@
+use super::constants::*;
+
+/// Just an alias for word to be more descriptive when referring to a location
+pub type MemoryLocation = u64;
+/// The standard unit of the system == 8 bytes == 64 bit
+pub type Word = u64;
+/// An alias for the u8 type to signal when working with data
+pub type Byte = u8;
+pub type WordByteArray = [Byte; WORD_SIZE];
