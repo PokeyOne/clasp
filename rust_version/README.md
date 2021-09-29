fdb06eeef469059125615b7613ecdfe80bbe8b5e
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-28 21:31:24 -0700

    Cargo initialization, rust format rules, git ignore, and source word and byte starting point

--- rust_version/README.md
+++ rust_version/README.md
@@ -1,1 +1,5 @@
-# Title
+# Clasp Rust Runtime Environment
+
+This is the Clasp runtime environment implemented in Rust. This version is an
+experiment that may or may not overtake the C-based implementation as it has
+around the same performance, is more safety oriented, and is more portable.
---
+++ rust_version/src/memory.rs
@@ -0,0 +1,48 @@
+use std::convert::TryInto;
+
+pub type MemoryLocation = u64;
+pub type Word = u64;
+pub type Byte = u8;
+
+trait ByteCollection {
+    fn from_bytes(bytes: Vec<Byte>) -> Word;
+    fn get_bytes(&self) -> Vec<Byte>;
+}
+
+impl ByteCollection for Word {
+    fn from_bytes(bytes: Vec<Byte>) -> Word {
+        if bytes.len() != 4 {
+            panic!() // TODO: Handle this properly
+        }
+
+        ((((((bytes[0] << 8) + bytes[1]) << 8) + bytes[2]) << 8) + bytes[3]).into()
+    }
+
+    fn get_bytes(&self) -> Vec<Byte> {
+        let mut temp = self.clone();
+        let mut result: Vec<Byte> = Vec::new();
+
+
+        // THis is going to require casting
+        result.push((temp % 256).try_into().unwrap());
+        temp = temp >> 8;
+        result.push((temp % 256).try_into().unwrap());
+        temp = temp >> 8;
+        result.push((temp % 256).try_into().unwrap());
+        temp = temp >> 8;
+        result.push(temp.try_into().unwrap());
+
+        result
+    }
+}
+
+pub struct Memory {
+    memory_size: u64,
+    memory: Vec<Byte>
+}
+
+impl Memory {
+    pub fn read(location: MemoryLocation) -> Word {
+        return 0;
+    }
+}
