94329ae805846787330825b03bc2d0d6a7239cf6
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-30 16:41:41 -0700

    Instruction lookup tables

--- rust_version/src/instructions.rs
+++ rust_version/src/instructions.rs
@@ -1,6 +1,19 @@
+use memory::types::{Word};
+use phf::phf_map;
+
 pub enum InstructionType {
     NOP,
     MOV
 }
 
+use InstructionType::*;
 
+pub static INSTRUCTIONS: phf::Map<static Word, InstructionType> = phf_map! {
+    0x0000_0000_0000_0000 => NOP,
+    0x0000_0000_0000_0001 => MOV
+};
+
+pub static INSTRUCTION_CODES: phf::Map<InstructionType, Word> = phf_map! {
+    NOP => 0x0000_0000_0000_0000,
+    MOV => 0x0000_0000_0000_0001
+}
