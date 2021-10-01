561f6218458757570e6d742af5ad0b2294c771ab
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-30 17:47:41 -0700

    Added phf macros as a dependency and corrected errors in instructions.rs

--- rust_version/src/instructions.rs
+++ rust_version/src/instructions.rs
@@ -1,6 +1,6 @@
-use memory::types::{Word, MemoryLocation};
-use memory::constants::*;
-use memory::Memory;
+use crate::memory::types::{Word, MemoryLocation};
+use crate::memory::constants::*;
+use crate::memory::Memory;
 use phf::phf_map;
 
 pub enum InstructionType {
@@ -15,73 +15,73 @@ pub enum InstructionType {
 
 use InstructionType::*;
 
-pub static INSTRUCTIONS: phf::Map<static Word, InstructionType> = phf_map! {
-    0x0000_0000_0000_0000 => NOP,
-    0x0000_0000_0000_0001 => MOV,
-    0x0000_0000_0000_0002 => ADD,
-    0x0000_0000_0000_0003 => SUB,
-    0x0000_0000_0000_0004 => MUL,
-    0x0000_0000_0000_0005 => DIV,
-    0x0000_0000_0000_0006 => POW
+pub static INSTRUCTIONS: phf::Map<u64, InstructionType> = phf_map! {
+    0x0000_0000_0000_0000u64 => NOP,
+    0x0000_0000_0000_0001u64 => MOV,
+    0x0000_0000_0000_0002u64 => ADD,
+    0x0000_0000_0000_0003u64 => SUB,
+    0x0000_0000_0000_0004u64 => MUL,
+    0x0000_0000_0000_0005u64 => DIV,
+    0x0000_0000_0000_0006u64 => POW
 };
 
-pub static INSTRUCTION_CODES: phf::Map<InstructionType, Word> = phf_map! {
-    NOP => 0x0000_0000_0000_0000,
-    MOV => 0x0000_0000_0000_0001,
-    ADD => 0x0000_0000_0000_0002,
-    SUB => 0x0000_0000_0000_0003,
-    MUL => 0x0000_0000_0000_0004,
-    DIV => 0x0000_0000_0000_0005,
-    POW => 0x0000_0000_0000_0006
-}
-
 mod instruction_providers {
-    static INSTRUCTION_FUNCTIONS: phf::Map<InstructionType, fn(&mut Memory, &mut MemoryLocation)> phf_map! {
-        NOP => nop_provider,
-        MOV => mov_provider,
-        ADD => add_provider,
-        SUB => sub_provider,
-        MUL => mul_provider,
-        DIV => div_provider,
-        POW => pow_provider
-    }
+    use super::*;
 
-    pub fn perform(inst: InstructionType, memory: &mut Memory, program_counter: &mut MemoryLocation) {
-        INSTRUCTION_FUNCTIONS.get(inst)(memory, program_counter);
+    type InstructionProvider = fn(&mut Memory, &mut MemoryLocation);
+
+    static INSTRUCTION_FUNCTIONS: phf::Map<u64, InstructionProvider> = phf_map! {
+        0u64 => nop_provider,
+        1u64 => mov_provider,
+        2u64 => add_provider,
+        3u64 => sub_provider,
+        4u64 => mul_provider,
+        5u64 => div_provider,
+        6u64 => pow_provider
+    };
+
+    pub fn perform(inst: u64, memory: &mut Memory, program_counter: &mut MemoryLocation) {
+        let method: &InstructionProvider = INSTRUCTION_FUNCTIONS.get(&inst).expect("Unimplemented instruction");
+        method(memory, program_counter);
     }
 
     fn nop_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+        *pc += WORD_SIZE as u64;
         return;
     }
 
-    fn mov_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn mov_provider(mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
+
+        let source_location: Word; // TODO: read location
+        let source_value: Word; // TODO: get value at location
+
+        // TODO: get destination and set destination
         return;
     }
 
-    fn add_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn add_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
         return;
     }
 
-    fn sub_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn sub_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
         return;
     }
 
-    fn mul_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn mul_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
         return;
     }
 
-    fn div_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn div_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
         return;
     }
 
-    fn pow_provider(_mem: &mut Memory, _pc: &mut MemoryLocation) {
-        pc += WORD_SIZE;
+    fn pow_provider(_mem: &mut Memory, pc: &mut MemoryLocation) {
+        *pc += WORD_SIZE as u64;
         return;
     }
 }
