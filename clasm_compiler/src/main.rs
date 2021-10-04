0fdbaad086f2c47a164803d0c02df59df0dfbb63
Mateo Carreras <mateo.carreras@gmail.com>
2021-10-03 22:13:31 -0700

    Test file and clasm compiler reads lines

--- clasm_compiler/src/main.rs
+++ clasm_compiler/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: Command line arguments for things like output file location
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Expected command with only one argument with the path of the clasm source file");
+    }
+
+    let file_content = fs::read_to_string(&args[1])?;
+
+    for line in file_content.lines() {
+        println!("line: {}", line);
+    }
+
+    return Ok(());
 }
