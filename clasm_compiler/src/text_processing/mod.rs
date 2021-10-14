11cad4659b073feac0d898792a3eb444456ef151
Mateo Carreras <mateo.carreras@gmail.com>
2021-10-13 22:36:01 -0700

    comments

--- clasm_compiler/src/text_processing/mod.rs
+++ clasm_compiler/src/text_processing/mod.rs
@@ -28,7 +28,31 @@ impl Argument {
     }
 }
 
+/// Given the string token of the raw assembly code instruction argument, this
+/// will process that and return an argument object.
+///
+/// Arguments have 3 main types:
+/// - Literal value
+/// - Address value
+/// - Register name
+///
+/// An address is written as just a plain number: 34, 0xF3, 0b00101011, or 0o147
+///
+/// A literal value is written the same as an address but wrapped in round
+/// brackets: (34), (0xF3), (0b00101011), or (0o147)
+///
+/// And finally a register name is just the character string identifier of the
+/// register. Similar to before it can take several forms:
+/// - reg = The value in the register is used. (the name is an address)
+/// - (reg) = The actual address of the register.
+///
+/// Essentially anywhere that you see a register name it will be replaced with
+/// the address of the register.
 fn process_arg(val: &str) -> Option<Argument> {
+    // TODO: Have a function "get_reg_address" that will return an address if
+    //       the string given to it is a register name, but will return None
+    //       when not given the name of a register. Then just match it here.
+
     // This is a literal
     if val.chars().nth(0)? == '(' && val.chars().nth(val.len() - 1)? == ')' {
         // Recursively get this method to process the inside value and then
