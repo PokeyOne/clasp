76dc43352956abc8c8de05f64d3b491319645db6
Mateo Carreras <mateo.carreras@gmail.com>
2021-10-27 23:55:07 -0700

    clasm compiler --nodump and --dump options

--- clasm_compiler/src/main.rs
+++ clasm_compiler/src/main.rs
@@ -13,9 +13,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         NamedArgSpec::new("--output", true, Some(vec!["-o".to_string()])),
         NamedArgSpec::new("--hello", false, None),
         NamedArgSpec::new("--version", false, Some(vec!["-v".to_string()])),
+        NamedArgSpec::new("--nodump", false, None),
+        NamedArgSpec::new("--dump", false, None),
     ]);
     let mut output_file_location: String = "./a.out".to_string();
     let mut input_path: String = String::new();
+    let mut dump_byte_code: bool = false;
 
     for parg in pargs {
         println!("arugment: {:?}", parg);
@@ -33,6 +36,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         println!("Clasm Compiler Version {}\n", VERSION_STRING);
 
                         return Ok(());
+                    } else if n == "--nodump" {
+                        dump_byte_code = false;
+                    } else if n == "--dump" {
+                        dump_byte_code = true;
                     }
                 }
             }
@@ -117,7 +124,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Collected {} labels", labels.size());
     labels.print_ordered_list();
 
-    clasp_common::io::print_binary_vec(&resulting_byte_code);
+    if dump_byte_code {
+        clasp_common::io::print_binary_vec(&resulting_byte_code);
+    }
 
     match clasp_common::io::write_binary_file(resulting_byte_code, &output_file_location) {
         Ok(_) => println!("Successfully wrote to file {}", &output_file_location),
