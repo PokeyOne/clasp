12c6959494506f0b8590e09f83cc7bb03a4ed89d
Mateo Carreras <mateo.carreras@gmail.com>
2021-10-29 11:56:45 -0700

    AST Nodes store raw value properly

--- compiler/src/compiling/tokenization/ast/nodes.rs
+++ compiler/src/compiling/tokenization/ast/nodes.rs
@@ -1,7 +1,15 @@
+#[derive(Debug)]
+pub enum RawValue {
+    Int(Box<i64>),
+    String(Box<String>),
+    UnsignedInt(Box<u64>)
+}
+
+#[derive(Debug)]
 pub struct ASTNode {
     method_name: String,
     content: Option<Box<ASTNode>>,
-    raw: Option<String>
+    raw: Option<RawValue>,
 }
 
 impl ASTNode {
@@ -13,11 +21,38 @@ impl ASTNode {
         }
     }
 
-    pub fn raw(value: String) -> ASTNode {
+    pub fn raw(value: RawValue) -> ASTNode {
         ASTNode {
             method_name: "raw".to_string(),
             content: None,
             raw: Some(value)
         }
+    }
+
+    pub fn iraw(value: i64) -> ASTNode {
+        ASTNode::raw(RawValue::Int(Box::new(value)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_raw_node() {
+        let node = ASTNode::raw(RawValue::Int(Box::new(3)));
+
+        assert_eq!("raw", node.method_name);
+        assert_eq!(true, node.content.is_none());
+        assert_eq!(3, match node.raw.unwrap() { RawValue::Int(val) => *val, _ => panic!("node was not integer") });
+    }
+
+    #[test]
+    fn create_raw_integer_node() {
+        let node = ASTNode::iraw(3);
+
+        assert_eq!("raw", node.method_name);
+        assert_eq!(true, node.content.is_none());
+        assert_eq!(3, match node.raw.unwrap() { RawValue::Int(val) => *val, _ => panic!("node was not integer") });
     }
 }
