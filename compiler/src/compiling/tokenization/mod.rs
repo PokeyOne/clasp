84e509c3a0b02562c1137969155ab029377d0c8f
Mateo Carreras <mateo.carreras@gmail.com>
2021-10-31 20:17:49 -0700

    work on string tracker to parse string. Probably should replace with iterator though

--- compiler/src/compiling/tokenization/mod.rs
+++ compiler/src/compiling/tokenization/mod.rs
@@ -1,8 +1,16 @@
 pub mod ast;
+pub mod string_tracker;
 
 use ast::AbstractSyntaxTree;
 use ast::nodes::ASTNode;
+use string_tracker::StringTracker;
 
 pub fn tokenize_text(input: String) -> AbstractSyntaxTree {
+    let base_nodes: Vec<ASTNode> = process_node_list(StringTracker::new(input, 0));
+
     return AbstractSyntaxTree::new(ASTNode::iraw(5));
+}
+
+fn process_node_list(tracker: StringTracker) -> Vec<ASTNode> {
+    return Vec::new();
 }
---
+++ compiler/src/compiling/tokenization/string_tracker.rs
@@ -0,0 +1,113 @@
+pub struct StringTracker {
+    data: String,
+    index: usize
+}
+
+// Haha, why not just use a fucking iterator
+impl StringTracker {
+    pub fn new(data: String, index: usize) -> StringTracker {
+        StringTracker { data: data, index: index }
+    }
+
+    pub fn increment(&mut self) {
+        self.fwd(1);
+    }
+
+    pub fn fwd(&mut self, amount: usize) {
+        self.index += amount;
+    }
+
+    pub fn chars_left(&self) -> usize {
+        self.data.len() - self.index
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn is_space(&self) -> bool {
+        match self.data.chars().nth(self.index) {
+            Some(val) => val.is_whitespace(),
+            None => false
+        }
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        self.data.chars().nth(self.index)
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        match self.peek() {
+            Some(val) => {
+                self.increment();
+                Some(val)
+            }
+            None => None
+        }
+    }
+
+    pub fn read(&mut self) -> Option<char> {
+        self.pop()
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        while self.is_space() {
+            self.increment();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_string_tracker() {
+        let tracker = StringTracker::new("bogus".to_string(), 0);
+        assert_eq!(5, tracker.chars_left());
+        assert_eq!(5, tracker.len());
+
+        let tracker = StringTracker::new("bogus".to_string(), 1);
+        assert_eq!(4, tracker.chars_left());
+        assert_eq!(5, tracker.len());
+    }
+
+    #[test]
+    fn go_forward_in_data() {
+        let mut tracker = StringTracker::new("b ogus".to_string(), 0);
+
+        tracker.increment();
+        assert_eq!(6, tracker.len());
+        assert_eq!(5, tracker.chars_left());
+        assert_eq!(true, tracker.is_space());
+
+        tracker.fwd(2);
+        assert_eq!(6, tracker.len());
+        assert_eq!(3, tracker.chars_left());
+        assert_eq!(false, tracker.is_space());
+    }
+
+    #[test]
+    fn peek_and_pop() {
+        let mut tracker = StringTracker::new("bogus".to_string(), 0);
+
+        assert_eq!(Some('b'), tracker.peek());
+        assert_eq!(5, tracker.chars_left());
+        assert_eq!(Some('b'), tracker.pop());
+        assert_eq!(4, tracker.chars_left());
+        assert_eq!(Some('o'), tracker.read());
+        assert_eq!(3, tracker.chars_left());
+    }
+
+    #[test]
+    fn read_to_end() {
+        let mut tracker = StringTracker::new("this is a message from the overlords".to_string(), 0);
+
+
+    }
+
+    #[test]
+    fn empty_tracker() {
+
+    }
+}
