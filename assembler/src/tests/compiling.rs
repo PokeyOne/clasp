92506088718981866b2ed3ef042beee19aa40a96
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-08 22:20:06 -0800

    move address test

--- assembler/src/tests/compiling.rs
+++ assembler/src/tests/compiling.rs
@@ -27,3 +27,18 @@ fn single_mov_with_literal_src() {
 
     assert_eq!(expected, calculated);
 }
+
+#[test]
+fn single_mov_with_address_src() {
+    let mut expected: Vec<u8> = CCLASP_SIGNATURE.to_vec();
+    let mut simple_expected: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0x20
+    ];
+    expected.append(&mut simple_expected);
+    let input_text = "mov 0xFFFF 0x20".to_string();
+    let calculated = compiling::compile_text(input_text);
+
+    assert_eq!(expected, calculated);
+}
