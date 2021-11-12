0953e400e00bb299a7bd43d491d5e6c9b6d94c99
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-12 03:08:53 -0800

    use data_block macro

--- assembler/src/text_processing/console_instructions/tests.rs
+++ assembler/src/text_processing/console_instructions/tests.rs
@@ -1,16 +1,12 @@
 use super::*;
-use crate::string_vec;
+use crate::{string_vec, data_block};
 
 #[test]
 fn basic_outr_literal_instruction() -> Result<(), OpProcessError> {
     let tokens: Vec<String> = string_vec!["outr", "(0x48)"];
+    let expected_data: Vec<u8> = data_block![0xA, 0x48];
 
     let (data, future_label_references) = outr_process(tokens)?;
-
-    let expected_data: Vec<u8> = vec![
-        0, 0, 0, 0, 0, 0, 0, 0x0A,
-        0, 0, 0, 0, 0, 0, 0, 0x48
-    ];
 
     assert_eq!(expected_data, data);
     assert_eq!(0, future_label_references.len());
@@ -21,13 +17,9 @@ fn basic_outr_literal_instruction() -> Result<(), OpProcessError> {
 #[test]
 fn basic_outr_address_instruction() -> Result<(), OpProcessError> {
     let tokens: Vec<String> = string_vec!["outr", "0x4863"];
+    let expected_data = data_block![0x09, 0x4863];
 
     let (data, future_label_references) = outr_process(tokens)?;
-
-    let expected_data: Vec<u8> = vec![
-        0, 0, 0, 0, 0, 0, 0, 0x09,
-        0, 0, 0, 0, 0, 0, 0x48, 0x63
-    ];
 
     assert_eq!(expected_data, data);
     assert_eq!(0, future_label_references.len());
@@ -38,13 +30,9 @@ fn basic_outr_address_instruction() -> Result<(), OpProcessError> {
 #[test]
 fn outr_address_max() -> Result<(), OpProcessError> {
     let tokens: Vec<String> = string_vec!["outr", "0xffffffffffffffff"];
+    let expected_data = data_block![9, 0xFFFF_FFFF_FFFF_FFFF];
 
     let (data, future_label_references) = outr_process(tokens)?;
-
-    let expected_data: Vec<u8> = vec![
-        0, 0, 0, 0, 0, 0, 0, 0x09,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-    ];
 
     assert_eq!(expected_data, data);
     assert_eq!(0, future_label_references.len());
@@ -65,13 +53,9 @@ fn outr_address_overflow() {
 #[test]
 fn outr_with_register() -> Result<(), OpProcessError> {
     let tokens: Vec<String> = string_vec!["outr", "ga"];
+    let expected_data = data_block![9, 0x8000_0000_0000_0000];
 
     let (data, future_label_references) = outr_process(tokens)?;
-
-    let expected_data: Vec<u8> = vec![
-        0, 0, 0, 0, 0, 0, 0, 0x09,
-        0x80, 0, 0, 0, 0, 0, 0, 0
-    ];
 
     assert_eq!(expected_data, data);
     assert_eq!(0, future_label_references.len());
