4eb800a606556a29c972f0ab9bb60cbeb267cd63
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-12 01:53:50 -0800

    outr tests

--- assembler/src/text_processing/console_instructions/tests.rs
+++ assembler/src/text_processing/console_instructions/tests.rs
@@ -1,4 +1,94 @@
+use super::*;
+
 #[test]
-fn it_works() {
-    assert!(true);
+fn basic_outr_literal_instruction() -> Result<(), OpProcessError> {
+    let tokens: Vec<String> = vec!["outr", "(0x48)"]
+        .iter()
+        .map(|element| element.to_string())
+        .collect();
+
+    let (data, future_label_references) = outr_process(tokens)?;
+
+    let expected_data: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 0x0A,
+        0, 0, 0, 0, 0, 0, 0, 0x48
+    ];
+
+    assert_eq!(expected_data, data);
+    assert_eq!(0, future_label_references.len());
+
+    Ok(())
+}
+
+#[test]
+fn basic_outr_address_instruction() -> Result<(), OpProcessError> {
+    let tokens: Vec<String> = vec!["outr", "0x4863"]
+        .iter()
+        .map(|element| element.to_string())
+        .collect();
+
+    let (data, future_label_references) = outr_process(tokens)?;
+
+    let expected_data: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 0x09,
+        0, 0, 0, 0, 0, 0, 0x48, 0x63
+    ];
+
+    assert_eq!(expected_data, data);
+    assert_eq!(0, future_label_references.len());
+
+    Ok(())
+}
+
+#[test]
+fn outr_address_max() -> Result<(), OpProcessError> {
+    let tokens: Vec<String> = vec!["outr", "0xffffffffffffffff"]
+        .iter()
+        .map(|element| element.to_string())
+        .collect();
+
+    let (data, future_label_references) = outr_process(tokens)?;
+
+    let expected_data: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 0x09,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+    ];
+
+    assert_eq!(expected_data, data);
+    assert_eq!(0, future_label_references.len());
+
+    Ok(())
+}
+
+#[test]
+fn outr_address_overflow() {
+    let tokens: Vec<String> = vec!["outr", "0xffaaffaaffaaffaa1"]
+        .iter()
+        .map(|element| element.to_string())
+        .collect();
+
+    match outr_process(tokens) {
+        Ok(_) => panic!("an argument greater than 64 bits should cause overflow"),
+        Err(_) => {}
+    }
+}
+
+#[test]
+fn outr_with_register() -> Result<(), OpProcessError> {
+    let tokens: Vec<String> = vec!["outr", "ga"]
+        .iter()
+        .map(|element| element.to_string())
+        .collect();
+
+    let (data, future_label_references) = outr_process(tokens)?;
+
+    let expected_data: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 0x09,
+        0x80, 0, 0, 0, 0, 0, 0, 0
+    ];
+
+    assert_eq!(expected_data, data);
+    assert_eq!(0, future_label_references.len());
+
+    Ok(())
 }
--- common/src/data_constants.rs
+++ common/src/data_constants.rs
@@ -4,63 +4,41 @@ use super::data_types::*;
 pub const WORD_SIZE: usize = 8;
 
 #[allow(dead_code)]
-pub const REGISTER_COUNT: u64 = 26;
-#[allow(dead_code)]
-pub const REGISTER_MEMORY_START: MemoryLocation = 0x8000_0000_0000_0000;
-#[allow(dead_code)]
-pub const RMS: MemoryLocation = REGISTER_MEMORY_START;
-#[allow(dead_code)]
-pub const GA_LOC: MemoryLocation = RMS + (WORD_SIZE * 0) as u64;
-#[allow(dead_code)]
-pub const GB_LOC: MemoryLocation = RMS + (WORD_SIZE * 1) as u64;
-#[allow(dead_code)]
-pub const GC_LOC: MemoryLocation = RMS + (WORD_SIZE * 2) as u64;
-#[allow(dead_code)]
-pub const GD_LOC: MemoryLocation = RMS + (WORD_SIZE * 3) as u64;
-#[allow(dead_code)]
-pub const GE_LOC: MemoryLocation = RMS + (WORD_SIZE * 4) as u64;
-#[allow(dead_code)]
-pub const GF_LOC: MemoryLocation = RMS + (WORD_SIZE * 5) as u64;
-#[allow(dead_code)]
-pub const GG_LOC: MemoryLocation = RMS + (WORD_SIZE * 6) as u64;
-#[allow(dead_code)]
-pub const GH_LOC: MemoryLocation = RMS + (WORD_SIZE * 7) as u64;
-#[allow(dead_code)]
-pub const GI_LOC: MemoryLocation = RMS + (WORD_SIZE * 8) as u64;
-#[allow(dead_code)]
-pub const GJ_LOC: MemoryLocation = RMS + (WORD_SIZE * 9) as u64;
-#[allow(dead_code)]
-pub const GK_LOC: MemoryLocation = RMS + (WORD_SIZE * 10) as u64;
-#[allow(dead_code)]
-pub const GL_LOC: MemoryLocation = RMS + (WORD_SIZE * 11) as u64;
-#[allow(dead_code)]
-pub const GM_LOC: MemoryLocation = RMS + (WORD_SIZE * 12) as u64;
-#[allow(dead_code)]
-pub const GN_LOC: MemoryLocation = RMS + (WORD_SIZE * 13) as u64;
-#[allow(dead_code)]
-pub const GO_LOC: MemoryLocation = RMS + (WORD_SIZE * 14) as u64;
-#[allow(dead_code)]
-pub const GP_LOC: MemoryLocation = RMS + (WORD_SIZE * 15) as u64;
-#[allow(dead_code)]
-pub const GQ_LOC: MemoryLocation = RMS + (WORD_SIZE * 16) as u64;
-#[allow(dead_code)]
-pub const GR_LOC: MemoryLocation = RMS + (WORD_SIZE * 17) as u64;
-#[allow(dead_code)]
-pub const GS_LOC: MemoryLocation = RMS + (WORD_SIZE * 18) as u64;
-#[allow(dead_code)]
-pub const GT_LOC: MemoryLocation = RMS + (WORD_SIZE * 19) as u64;
-#[allow(dead_code)]
-pub const GU_LOC: MemoryLocation = RMS + (WORD_SIZE * 20) as u64;
-#[allow(dead_code)]
-pub const GV_LOC: MemoryLocation = RMS + (WORD_SIZE * 21) as u64;
-#[allow(dead_code)]
-pub const GW_LOC: MemoryLocation = RMS + (WORD_SIZE * 22) as u64;
-#[allow(dead_code)]
-pub const GX_LOC: MemoryLocation = RMS + (WORD_SIZE * 23) as u64;
-#[allow(dead_code)]
-pub const GY_LOC: MemoryLocation = RMS + (WORD_SIZE * 24) as u64;
-#[allow(dead_code)]
-pub const GZ_LOC: MemoryLocation = RMS + (WORD_SIZE * 25) as u64;
+mod register_locations {
+    use super::{WORD_SIZE, MemoryLocation};
+
+    pub const REGISTER_COUNT: u64 = 26;
+    pub const REGISTER_MEMORY_START: MemoryLocation = 0x8000_0000_0000_0000;
+    pub const RMS: MemoryLocation = REGISTER_MEMORY_START;
+    pub const GA_LOC: MemoryLocation = RMS + (WORD_SIZE * 0) as u64;
+    pub const GB_LOC: MemoryLocation = RMS + (WORD_SIZE * 1) as u64;
+    pub const GC_LOC: MemoryLocation = RMS + (WORD_SIZE * 2) as u64;
+    pub const GD_LOC: MemoryLocation = RMS + (WORD_SIZE * 3) as u64;
+    pub const GE_LOC: MemoryLocation = RMS + (WORD_SIZE * 4) as u64;
+    pub const GF_LOC: MemoryLocation = RMS + (WORD_SIZE * 5) as u64;
+    pub const GG_LOC: MemoryLocation = RMS + (WORD_SIZE * 6) as u64;
+    pub const GH_LOC: MemoryLocation = RMS + (WORD_SIZE * 7) as u64;
+    pub const GI_LOC: MemoryLocation = RMS + (WORD_SIZE * 8) as u64;
+    pub const GJ_LOC: MemoryLocation = RMS + (WORD_SIZE * 9) as u64;
+    pub const GK_LOC: MemoryLocation = RMS + (WORD_SIZE * 10) as u64;
+    pub const GL_LOC: MemoryLocation = RMS + (WORD_SIZE * 11) as u64;
+    pub const GM_LOC: MemoryLocation = RMS + (WORD_SIZE * 12) as u64;
+    pub const GN_LOC: MemoryLocation = RMS + (WORD_SIZE * 13) as u64;
+    pub const GO_LOC: MemoryLocation = RMS + (WORD_SIZE * 14) as u64;
+    pub const GP_LOC: MemoryLocation = RMS + (WORD_SIZE * 15) as u64;
+    pub const GQ_LOC: MemoryLocation = RMS + (WORD_SIZE * 16) as u64;
+    pub const GR_LOC: MemoryLocation = RMS + (WORD_SIZE * 17) as u64;
+    pub const GS_LOC: MemoryLocation = RMS + (WORD_SIZE * 18) as u64;
+    pub const GT_LOC: MemoryLocation = RMS + (WORD_SIZE * 19) as u64;
+    pub const GU_LOC: MemoryLocation = RMS + (WORD_SIZE * 20) as u64;
+    pub const GV_LOC: MemoryLocation = RMS + (WORD_SIZE * 21) as u64;
+    pub const GW_LOC: MemoryLocation = RMS + (WORD_SIZE * 22) as u64;
+    pub const GX_LOC: MemoryLocation = RMS + (WORD_SIZE * 23) as u64;
+    pub const GY_LOC: MemoryLocation = RMS + (WORD_SIZE * 24) as u64;
+    pub const GZ_LOC: MemoryLocation = RMS + (WORD_SIZE * 25) as u64;
+}
+
+pub use register_locations::*;
 
 // TODO: Consider just an enum Register
 
