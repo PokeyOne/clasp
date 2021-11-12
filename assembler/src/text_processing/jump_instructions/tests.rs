787aa1dd8064b390b772459409e8a81ce99bb73d
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-12 02:58:47 -0800

    two additional test of the jump instruction compilation

--- assembler/src/text_processing/jump_instructions/tests.rs
+++ assembler/src/text_processing/jump_instructions/tests.rs
@@ -14,3 +14,22 @@ fn basic_jmp_label() -> Result<(), OpProcessError> {
 
     Ok(())
 }
+
+#[test]
+fn jump_with_wrong_number_of_args() {
+    assert!(jmp_process(string_vec!["jmp", ":main", ":function"]).is_err());
+    assert!(jmp_process(string_vec!["jmp"]).is_err());
+}
+
+#[test]
+fn jump_with_address() -> Result<(), OpProcessError> {
+    let tokens = string_vec!["jmp", "0x18"];
+    let expected_data: Vec<u8> = data_block![11, 0x18];
+
+    let (data, future_label_refs) = jmp_process(tokens)?;
+
+    assert_eq!(expected_data, data);
+    assert_eq!(0, future_label_refs.len());
+
+    Ok(())
+}
