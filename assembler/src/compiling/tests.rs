beb37d757e2bed1987e87b9ff5da4c57cf185376
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-12 23:06:50 -0800

    two test for single return and call instruction compilation and new CCLASP_SIGNATURE_WORD const

--- assembler/src/compiling/tests.rs
+++ assembler/src/compiling/tests.rs
@@ -1,5 +1,5 @@
-use crate::compiling;
-use clasp_common::io::CCLASP_SIGNATURE;
+use crate::{compiling, data_block};
+use clasp_common::io::{CCLASP_SIGNATURE, CCLASP_SIGNATURE_WORD};
 
 #[test]
 fn single_nop() {
@@ -56,6 +56,33 @@ fn nop_with_comments() {
     ];
     expected.append(&mut simple_expected);
     let input_text = ";; this line is a comment\nnop\n;; haha nope\nnop ;; with comment at end of line\nnop\n;; comment at end of the file\n;; haha, a multiline comment\n\n\n;; blah".to_string();
+    let calculated = compiling::compile_text(input_text);
+
+    assert_eq!(expected, calculated);
+}
+
+#[test]
+fn single_call() {
+    let input_text = "nop
+:main
+call :main
+end".to_string();
+    let expected: Vec<u8> = data_block![
+        CCLASP_SIGNATURE_WORD, 0x00, 0x0D, 0x10, 0x07
+    ];
+
+    let calculated = compiling::compile_text(input_text);
+
+    assert_eq!(expected, calculated);
+}
+
+#[test]
+fn single_return() {
+    let input_text = "return".to_string();
+    let expected: Vec<u8> = data_block![
+        CCLASP_SIGNATURE_WORD, 0x0E
+    ];
+
     let calculated = compiling::compile_text(input_text);
 
     assert_eq!(expected, calculated);
--- common/src/io.rs
+++ common/src/io.rs
@@ -8,15 +8,19 @@ use std::fs;
 /// There is also two reserved bytes after the signature to keep things 8-byte
 /// aligned
 pub const CCLASP_SIGNATURE: [Byte; 8] = [
-    AsciiChar::C as u8,
-    AsciiChar::L as u8,
-    AsciiChar::A as u8,
-    AsciiChar::S as u8,
-    AsciiChar::P as u8,
+    AsciiChar::C as u8, // 0x43
+    AsciiChar::L as u8, // 0x4C
+    AsciiChar::A as u8, // 0x41
+    AsciiChar::S as u8, // 0x53
+    AsciiChar::P as u8, // 0x50
     0x00u8,
     0x00u8,
     0x00u8
 ];
+
+/// Identical to the CCLASP_SGINATURE constant, but in the form of a single
+/// word instead of an array
+pub const CCLASP_SIGNATURE_WORD: u64 = 0x434C_4153_5000_0000;
 
 pub fn write_binary_file(data: Vec<Byte>, path: &String) -> Result<(), Box<dyn std::error::Error>> {
     fs::write(path, &data[0..])?;
