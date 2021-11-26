ccbd0b22cea4eedbec949ba349b29155a9377376
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-26 01:34:03 -0800

    test for word to bytes

--- common/src/data_types/tests.rs
+++ common/src/data_types/tests.rs
@@ -7,3 +7,12 @@ fn word_to_bytes() {
 
     assert_eq!(word.to_bytes(), expected_bytes);
 }
+
+#[test]
+fn bytes_to_words() {
+    let bytes: [u8; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+    let expected_word: Word = 0x0102_0304_0506_0708;
+
+    assert_eq!(Word::from_bytes(&bytes), expected_word);
+    assert_eq!(Word::from_bytes_v(&bytes.to_vec()), expected_word);
+}
