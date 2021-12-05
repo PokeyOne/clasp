d7d46a6a609364d54506522c72e6ce1de4af109c
Mateo Carreras <mateo.carreras@gmail.com>
2021-12-04 20:34:51 -0800

    test for single bracket tokenization

--- parsing/src/tokenization/tests/brackets.rs
+++ parsing/src/tokenization/tests/brackets.rs
@@ -5,4 +5,15 @@ use clasp_common::string_vec;
 #[test]
 fn test_single_brackets() {
     let input = string_vec!["(", ")", "[", "]", "{", "}"];
+    let expected = vec![
+        vec![Token::OpenBracket],
+        vec![Token::CloseBracket],
+        vec![Token::OpenSquareBracket],
+        vec![Token::CloseSquareBracket],
+        vec![Token::OpenCurlyBracket],
+        vec![Token::CloseCurlyBracket]
+    ];
+
+    let calculated: Vec<Vec<Token>> = input.iter().map(|ele| tokenize(&ele).unwrap()).collect();
+    assert_eq!(expected, calculated);
 }
